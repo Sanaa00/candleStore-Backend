1aaa1d443abda48c3036a2e9b16ddb8e0f034583
Sana Rzgar <sanarizgar34@gmail.com>
2023-04-29 15:29:46 +0300

    models

--- models/cart.models.js
+++ models/cart.models.js
@@ -1,23 +1,23 @@
 import mongoose from "mongoose";
 
 const cartSchema = new mongoose.Schema({
-  productName: { type: String, required: true },
-  description: { type: String, required: true },
-  price: { type: Number, required: true },
-  color: [String],
-  images: { type: [String] },
-  raiting: { type: Number, default: 1 },
-  quantity: { type: Number, default: 1 },
-  status: { type: String },
+  // productName: { type: String, required: true },
+  // description: { type: String, required: true },
+  // price: { type: Number, required: true },
+  // color: [String],
+  // images: { type: [String] },
+  // raiting: { type: Number, default: 1 },
+  // quantity: { type: Number, default: 1 },
+  status: { type: String, default: "cart" },
   // address: { type: String },
-  review: String,
-  category: String,
+  // review: String,
+  // category: String,
 
-  user: { type: mongoose.Types.ObjectId, ref: "user", required: true },
-  adress: { type: mongoose.Types.ObjectId, ref: "address", required: true },
+  user: { type: mongoose.Types.ObjectId, ref: "user" },
+  address: { type: mongoose.Types.ObjectId, ref: "address" },
 
   // order: { type: mongoose.Types.ObjectId, ref: "order" },
-  products: [{ type: mongoose.Types.ObjectId, ref: "product", required: true }],
+  products: [{ type: mongoose.Types.ObjectId, ref: "product" }],
 });
 const cart = mongoose.model("cart", cartSchema);
 export default cart;
